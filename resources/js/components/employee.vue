8abbd24ba4a40030e1862d707f83f4cd0b1bad35
Rabib Galib <rabib@genex.digital>
2022-06-11 19:25:34 +0600

    employee crud done

--- resources/js/components/employee.vue
+++ resources/js/components/employee.vue
@@ -2,7 +2,7 @@
     <div class="container p-5">
 
         <button type="button" class="btn btn-primary mb-4 float-right"
-        data-toggle="modal" data-target="#myModal">
+        data-toggle="modal" data-target="#myModal" @click="resetForm">
             New Employee
         </button>
 
@@ -36,8 +36,11 @@
                             <input type="text" class="form-control"  v-model="form.phone">
                         </div>
 
-                        <button type="button" class="btn btn-primary"
-                        @click="storeEmployee">Submit</button>
+                        <button v-if="employee_id == ''" type="button" class="btn btn-primary"
+                                @click="storeEmployee">Submit</button>
+
+                        <button v-else type="button" class="btn btn-primary"
+                                @click="updateEmployee">Update</button>
 
                     </div>
 
@@ -61,7 +64,16 @@
                         <td>{{ employee.name }}</td>
                         <td>{{ employee.email }}</td>
                         <td>{{ employee.phone }}</td>
-                        <td>Edit</td>
+                        <td>
+                            <button type="button" class="btn btn-sm btn-info mr-2"
+                                    data-toggle="modal" data-target="#myModal" @click="editEmployee(employee)">
+                                Edit
+                            </button>
+
+                            <button type="button" class="btn btn-sm btn-info" @click="deleteEmployee(employee.id)">
+                                Delete
+                            </button>
+                        </td>
                     </tr>
                 </template>
 
@@ -80,6 +92,7 @@
         setup() {
             const api = "http://localhost:8000/";
             const employees = ref([]);
+            const employee_id = ref([]);
             const errors = ref([]);
 
             const form = reactive({
@@ -89,15 +102,15 @@
             });
 
             const getEmployees = async() => {
-                console.log(api);
+                // console.log(api);
                 let res = await axios.get(api +'api/employees');
                 employees.value = res.data;
             }
 
             const storeEmployee = async() => {
                 try {
-                    await axios.post('api/employee', form)
-                    await getEmployees()
+                    await axios.post(api+'api/employee', form)
+                    getEmployees()
                     resetForm()
                     $("#myModal").modal('hide');
                 } catch(e) {
@@ -112,9 +125,40 @@
                 }
             }
 
+            const updateEmployee = async() => {
+                try {
+                    await axios.patch(api+'api/employee/'+employee_id.value, form)
+                    getEmployees()
+                    resetForm()
+                    $("#myModal").modal('hide');
+                } catch(e) {
+                    console.log(e);
+                    if (e.response.status === 422) {
+                        const data = [];
+                        for (const key in e.response.data.errors) {
+                            data.push(e.response.data.errors[key][0]);
+                        }
+                        errors.value = data;
+                    }
+                }
+            }
+
+            const editEmployee = (employee) => {
+                employee_id.value = employee.id;
+                form.name = employee.name;
+                form.email = employee.email;
+                form.phone = employee.phone;
+            }
+
+            const deleteEmployee = async(id) => {
+                await axios.delete(api + 'api/employee/' + id);
+                getEmployees();
+            }
+
             onMounted(getEmployees());
 
             const resetForm = () => {
+                employee_id.value = '';
                 form.name = '';
                 form.email = '';
                 form.phone = '';
@@ -124,7 +168,12 @@
                 employees,
                 form,
                 storeEmployee,
-                errors
+                errors,
+                editEmployee,
+                updateEmployee,
+                employee_id,
+                resetForm,
+                deleteEmployee
             }
 
         }
